866b9ff4946f1a4d4b63ed01468378be487b0885
Amit Patange <amitpatange88@gmail.com>
2019-01-17 00:36:41 +0530

    iframe issue

---
+++ js/config.js
@@ -0,0 +1,13 @@
+var config  = {
+  Script: "Javascript",
+  Version : "v1.0",
+  LoadViewPort : function() {
+  		console.log("LoadViewPort");
+  		var meta = document.createElement('meta');
+  		meta.name = "viewport";
+		meta.content = "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0;";
+		//<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0;">
+		document.getElementsByTagName('head')[0].appendChild(meta);
+    	console.log(meta);
+  }
+};
